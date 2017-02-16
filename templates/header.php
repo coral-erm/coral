c47e754fcbb48752c009d80a4b5b415fe4ad3fcf
Connor de la Cruz <connor.c.delacruz@gmail.com>
2017-02-16 16:53:35 -0600

    Added more simple common module header elements and TODO comments for places where things vary between modules

--- templates/header.php
+++ templates/header.php
@@ -36,3 +36,31 @@ $coralURL = $util->getCORALURL();
 
 ?>
 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <?php // TODO: resources and organizations have <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="public"> ?>
+    <title>
+        <?php // TODO: $moduleTitle set similarly to $pageTitle
+        echo _("Usage Statistics") . ' - ' . $pageTitle; ?>
+    </title>
+    <?php // TODO: stylesheet and script imports vary between modules ?>
+    <?php
+    // Add translation for the JavaScript files
+    global $http_lang;
+    $str = substr($_SERVER["HTTP_ACCEPT_LANGUAGE"],0,5);
+    $default_l = $lang_name->getLanguage($str);
+    if($default_l==null || empty($default_l)){$default_l=$str;}
+    if(isset($_COOKIE["lang"])){
+        if($_COOKIE["lang"]==$http_lang && $_COOKIE["lang"] != "en_US"){
+            echo "<link rel='gettext' type='application/x-po' href='./locale/".$http_lang."/LC_MESSAGES/messages.po' />";
+        }
+    }else if($default_l==$http_lang && $default_l != "en_US"){
+        echo "<link rel='gettext' type='application/x-po' href='./locale/".$http_lang."/LC_MESSAGES/messages.po' />";
+    }
+    ?>
+    <?php // TODO: more varying script imports ?>
+</head>
+<?php // TODO: body ?>
+
