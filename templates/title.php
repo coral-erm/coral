d6818d66063959b28cd5a731f865bd15dfbc2cd2
Connor de la Cruz <connor.c.delacruz@gmail.com>
2017-02-24 16:06:15 -0600

    More progress on templates/title.php

--- templates/title.php
+++ templates/title.php
@@ -134,24 +134,52 @@
                                         </a>
 
                                     </td>
-                                    <?php // TODO: end module-specific chunk ?>
+                                    <?php // TODO: end of module-specific chunk ?>
 
                                     <td style='width:130px;height:19px;' align='right'>
+                                        <?php
 
-                                        <div style='text-align:left;'>
-                                            <ul class="tabs">
-                                                <li id="change-mod-menu"><span>Change Module</span><i class="fa fa-chevron-down"></i>
-                                                    <ul class="coraldropdown">
-                                                        <li class="change-mod-item"><a href="http://localhost/Coral/" target='_blank' title="Main Menu"><img src='images/change/icon-mod-main.png'><span>Main Menu</span></a></li>
-                                                        <li class="change-mod-item"><a href="http://localhost/Coral/organizations/" target='_blank' title="Organizations module"><img src='images/change/icon-mod-organizations.png'><span>Organizations</span></a></li>
-                                                        <li class="change-mod-item"><a href="http://localhost/Coral/licensing/" target='_blank' title="Licensing module"><img src='images/change/icon-mod-licensing.png'><span>Licensing</span></a></li>
-                                                        <li class="change-mod-item"><a href="http://localhost/Coral/usage/" target='_blank' title="Usage Statistics module"><img src='images/change/icon-mod-usage.png'><span>Usage Statistics</span></a></li>
-                                                        <li class="change-mod-item"><a href="http://localhost/Coral/management/" target='_blank' title="Management module"><img src='images/change/icon-mod-management.png'><span>Management</span></a></li>
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </div>
+                                        //only show the 'Change Module' if there are other modules installed or if there is an index to the main CORAL page
+                                        if ((file_exists($util->getCORALPath() . "index.php")) || ($config->settings->licensingModule == 'Y') || ($config->settings->organizationsModule == 'Y') || ($config->settings->managementModule == 'Y') || ($config->settings->usageModule == 'Y')) {
 
+                                            ?>
+                                            <div style='text-align:left;'>
+                                                <ul class="tabs">
+                                                    <li id="change-mod-menu"><span><?php echo _("Change Module");?></span><i class="fa fa-chevron-down"></i>
+                                                        <ul class="coraldropdown">
+                                                            <?php if (file_exists($util->getCORALPath() . "index.php")) {?>
+                                                                <li class="change-mod-item"><a href="<?php echo $coralURL; ?>" target='_blank' title="<?php echo _("Main Menu"); ?>"><img src='images/change/icon-mod-main.png'><span><?php echo _("Main Menu");?></span></a></li>
+                                                                <?php
+                                                            }
+                                                            if ($config->settings->organizationsModule == 'Y') {
+                                                                ?>
+                                                                <li class="change-mod-item"><a href="<?php echo $coralURL; ?>organizations/" target='_blank' title="<?php echo _("Organizations module"); ?>"><img src='images/change/icon-mod-organizations.png'><span><?php echo _("Organizations");?></span></a></li>
+                                                                <?php
+                                                            }
+                                                            if ($config->settings->licensingModule == 'Y') {
+                                                                ?>
+                                                                <li class="change-mod-item"><a href="<?php echo $coralURL; ?>licensing/" target='_blank' title="<?php echo _("Licensing module"); ?>"><img src='images/change/icon-mod-licensing.png'><span><?php echo _("Licensing");?></span></a></li>
+                                                                <?php
+                                                            }
+                                                            if ($config->settings->usageModule == 'Y') {
+                                                                ?>
+                                                                <li class="change-mod-item"><a href="<?php echo $coralURL; ?>usage/" target='_blank' title="<?php echo _("Usage Statistics module"); ?>"><img src='images/change/icon-mod-usage.png'><span><?php echo _("Usage Statistics");?></span></a></li>
+                                                                <?php
+                                                            }
+                                                            if ($config->settings->managementModule == 'Y') {
+                                                                ?>
+                                                                <li class="change-mod-item"><a href="<?php echo $coralURL; ?>management/" target='_blank' title="<?php echo _("Management module"); ?>"><img src='images/change/icon-mod-management.png'><span><?php echo _("Management");?></span></a></li>
+                                                            <?php } ?>
+                                                        </ul>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                            <?php
+
+                                        } else {
+                                            echo "&nbsp;";
+                                        }
+                                        ?>
                                     </td>
                                 </tr>
                             </table>
@@ -169,4 +197,11 @@
                                     document.cookie ='lang='+lang+';path=/'+';domain='+wl.host+';expires='+now;
                                 }
                             </script>
-                            <span id='span_message' class='darkRedText' style='text-align:left;'></span>
+                            <span id='span_message' class='darkRedText' style='text-align:left;'>
+                                <?php
+                                if (isset($_POST['message']))
+                                    echo $_POST['message'];
+                                if (isset($errorMessage))
+                                    echo $errorMessage;
+                                ?>
+                            </span>
